b08fe07f73e4840ea7004b04374a35d3290e9b71
kieu hai dang <dangkh_58@vnu.edu.vn>
2017-04-01 18:56:37 +0700

    edit time send mail automaticaly

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -13,7 +13,7 @@ class Admin::StatisticsController < ApplicationController
     @products.each do |product, quantity|
       @data.append [product.name, quantity]
     end
-    StatisticsWorker.perform_at 1.minutes.from_now
+    StatisticsWorker.perform_at load_time
     @products = Kaminari.paginate_array(@products).page(params[:page])
       .per Settings.product.per_page
   end
