8897f79d220361eddb2ddffdea1052c8c1f24e4e
kieu hai dang <dangkh_58@vnu.edu.vn>
2017-04-04 11:14:23 +0700

    change 2

--- app/controllers/admin/post_admins_controller.rb
+++ app/controllers/admin/post_admins_controller.rb
@@ -1,17 +1,3 @@
 class Admin::PostAdminsController < ApplicationController
-  load_and_authorize_resource
-  before_action :verify_admin, :authenticate_user!
-
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
 
 end
