d9b5e5f3d6b22d6c5b4dc0098cb6bec3c9f1d8af
kieu hai dang <dangkh_58@vnu.edu.vn>
2017-04-04 17:07:28 +0700

    add js to post

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,9 @@
+$(document).on('ready page:load', function() {
+  if ($(".body-post").length > 0){
+    var $post = $( ".post" ),
+    str = $(".body-post").text()
+    html = $.parseHTML( str ),
+    $(".body-post").hide()
+    $post.append( html );
+  }
+})
