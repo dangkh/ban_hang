9e7b6eee0b31a3a63d2a3efa62d1933d26a68b50
kieu hai dang <dangkh_58@vnu.edu.vn>
2017-04-04 11:19:44 +0700

    update controller

--- app/controllers/admin/post_admins_controller.rb
+++ app/controllers/admin/post_admins_controller.rb
@@ -1,3 +1,24 @@
 class Admin::PostAdminsController < ApplicationController
+  load_and_authorize_resource
+  before_action :verify_admin, :authenticate_user!
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    tmp = PostAdmin.new post_admin_params
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+private
+
+  def post_admin_params
+    params.require(:post_admin).permit :body, :title
+  end
 end
